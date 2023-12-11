3c208b6305e618722411b3c9a961a0f41b9af49b
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2023-12-11 20:51:45 +0600

    Update schema.js to import teamPage schema

--- sanity/schema.js
+++ sanity/schema.js
@@ -2,19 +2,13 @@ import AboutUs from "./schemas/AboutUs";
 import Footer from "./schemas/Footer";
 import MainSec from "./schemas/MainSec";
 import SingleImageCard from "./schemas/SingleImageCard";
-import ScienceImage from "./schemas/ScienceImage";
-import ElectricalImage from "./schemas/ElectricalImage";
-import MechanicalImage from "./schemas/MechanicalImage";
-import SoftwareImage from "./schemas/SoftwareImage";
+
 import Sponsors from "./schemas/Sponsors";
-import ScienceTop from "./schemas/ScienceTop";
-import ElectricalTop from "./schemas/ElectricalTop";
-import MehcaTop from "./schemas/MehcaTop";
-import SoftwareTop from "./schemas/SoftwareTop";
-import Teamlist from "./schemas/Teamlist";
+
+import teamPage from "./schemas/TeamList";
 import Achievement from "./schemas/Achievement";
 import ContactUs from "./schemas/ContactUs";
 
 export const schema = {
-  types: [MainSec,AboutUs,Sponsors,Footer,SingleImageCard,Teamlist,Achievement,ContactUs],
+  types: [MainSec,AboutUs,Sponsors,Footer,SingleImageCard,teamPage,Achievement,ContactUs],
 }
