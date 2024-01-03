f557c8ff5eb2365cd28eea184e689f8a58d821e7
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2024-01-03 22:04:21 +0600

    Add carousel navigation buttons to ImageSlider component

--- components/ImageSlider.js
+++ components/ImageSlider.js
@@ -29,7 +29,10 @@ export function ImageSlider({ data }) {
                     ))}
 
                 </CarouselContent>
+                <CarouselPrevious />
+      <CarouselNext />
             </Carousel>
         </div>
     )
 }
+
