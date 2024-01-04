a4a1c11293aba65302f1eec70bfb2134d0a3a1fd
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2024-01-04 23:40:30 +0600

    Remove unnecessary text in DonateUs.js

--- components/DonateUs.js
+++ components/DonateUs.js
@@ -7,7 +7,7 @@ const DonateUs = () => {
             <h1 className='text-4xl font-bold mb-4'>Donate Us</h1>
             <p className='text-lg mb-8'>
 
-                You can donate us by clicking on the button below. We will be very thankful to you for your donation.
+                You can donate us by clicking on the button below. 
             </p>
             <Link href={"/donate"} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
                 Donate Now
