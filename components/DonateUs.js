567e7f66a0db5df846d5a7e52badfa5f5d47c5c5
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2024-01-04 23:30:43 +0600

    Update donation message in DonateUs component

--- components/DonateUs.js
+++ components/DonateUs.js
@@ -5,7 +5,10 @@ const DonateUs = () => {
     return (
         <div className='grid place-content-center text-center bg-[#161e30] pt-[3%] pb-[3%]'>
             <h1 className='text-4xl font-bold mb-4'>Donate Us</h1>
-            <p className='text-lg mb-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p className='text-lg mb-8'>
+
+                You can donate us by clicking on the button below. We will be very thankful to you for your donation.
+            </p>
             <Link href={"/donate"} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
                 Donate Now
             </Link>
