c096887c22b18ed2a23d12997f91e75684d6175f
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2024-01-05 16:23:08 +0600

    Remove unused imports and fix typo in achievement.jsx

--- components/component/achievment.jsx
+++ components/component/achievment.jsx
@@ -1,9 +1,7 @@
 import BlockContent from '@sanity/block-content-to-react'
 import { Badge } from '@/components/ui/badge'
 import client, { urlFor } from '@/lib/sanity'
-import SanityBlockContent from '@sanity/block-content-to-react'
 import Image from 'next/image'
-import { LucideHeading, LucideHeading3 } from 'lucide-react'
 export const revalidate = 3600
 async function loadData() {
   const query = `*[_type == "achievementsPage"]  | order(time desc)`
