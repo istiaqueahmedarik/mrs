d7f32ba97f44fe8b04834456bdec64774448d855
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2024-01-05 23:49:04 +0600

    Refactor team-lead component to replace Link with div

--- components/component/team-lead.jsx
+++ components/component/team-lead.jsx
@@ -10,11 +10,11 @@ export function TeamLead({data}) {
       className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-4">
         
         <div  className="flex flex-col items-center space-y-2">
-        <Link href="#">
+        <div>
           <Avatar>
             <AvatarImage alt="image" src={urlFor(data.avatarImage).url()} />
           </Avatar>
-        </Link>
+        </div>
         <h3 className="font-semibold">{data.name}</h3>
         <p className="text-sm text-gray-500 dark:text-gray-400">{data.role}</p>
       </div>
