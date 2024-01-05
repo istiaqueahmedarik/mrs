8ecfd4c3e9f5eb5e479cfa7ed4b4cf2b1ed59f70
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2024-01-05 15:37:38 +0600

    Add revalidate constant to layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import Script from 'next/script'
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Toaster } from '@/components/ui/toaster'
 const inter = Inter({ subsets: ['latin'] })
-
+export const revalidate = 3600
 export const metadata = {
   title: 'Mongor Barta',
   description: 'Mongor Barta is a club for developing and learning about mars rover and space science',
