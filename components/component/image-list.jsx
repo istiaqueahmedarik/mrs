28aa555d7ceec68b3b2e66c909fd27a74b25be7e
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2024-04-16 01:11:07 +0600

    Update styling in ImageList and Card components

--- components/component/image-list.jsx
+++ components/component/image-list.jsx
@@ -11,11 +11,11 @@ export async function ImageList({data}) {
 
   console.log(data)
   return (
-    (<div className="mb-[4%]">
+    (<div className="mb-[4%] ">
     <h2 className="text-2xl font-semibold mb-4 text-center">Media Outreach</h2>
     <p className="text-center mb-8">We have been featured in the following publications</p>
       <section
-      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4 md:p-6">
+      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4 md:p-6 bg-gray-950">
         {data.map((item,index) => {
           return(
             <HoverCard key={index}>
@@ -32,7 +32,7 @@ export async function ImageList({data}) {
                   objectFit: "cover",
                 }}
                 width="400" />
-              <div className=" p-4 dark:bg-gray-950">
+              <div className=" p-4 bg-gray-950">
                 <h3 className="font-semibold text-lg md:text-xl text-white bg-[#111827]">{item.title}</h3>
                 {/* <Button variant="link">Learn more</Button> */}
               </div>
--- components/ui/card.jsx
+++ components/ui/card.jsx
@@ -6,7 +6,7 @@ const Card = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn(
-      "rounded-xl border border-slate-200 bg-white text-slate-950 shadow dark:border-slate-800 dark:bg-slate-950 dark:text-slate-50",
+      "rounded-xl border border-slate-200 bg-gray-950 text-slate-950 shadow dark:border-slate-800 dark:bg-slate-950 dark:text-slate-50",
       className
     )}
     {...props} />
