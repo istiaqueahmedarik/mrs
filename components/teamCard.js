ea71a527a357afcf7615bbfb7290fb6aaa6a0756
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2024-06-01 02:10:37 +0600

    Update teamCard component to use higher resolution images

--- components/teamCard.js
+++ components/teamCard.js
@@ -6,16 +6,16 @@ import React from 'react'
 function TeamCard(props) {
   return (
       <div className="rounded-lg relative bg-gray-900 p-2 shadow-lg flex flex-auto justify-center">
-              <Image placeholder="blur" blurDataURL="/iconblur.jpg" quality={100}
+              <Image placeholder="blur" blurDataURL="/iconblur.jpg" 
                   alt={`${props.name}`}
               className="parent h-full w-full  transition-transform duration-300  object-cover m-auto scale-100 rounded-lg hover:scale-120 hover:blur-xl"
-                  height="100"
+                  height="1024"
                   src={urlFor(props.imageSrc).url()}
                   style={{
                       aspectRatio: "80/80",
                       objectFit: "cover",
                   }}
-                  width="80" />
+              width="1024" />
           <div className='child absolute inset-1 flex items-end p-4 opacity-0 hover:opacity-100 transition-all  hover:bg-[#07070743]'>
               <div className='flex flex-col'>
                   <Link href={`/member/${props.teamMemberSlug}/${props.year}`} className="mb-2 text-[100%] text-[#fcfcfc] font-extrabold">{`${props.name}`}</Link>
