75498fcaf70d539d1a0dabd30a59bcf45f08dcde
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2024-06-01 16:53:51 +0600

    Update teamCard component to use higher resolution images

--- components/teamCard.js
+++ components/teamCard.js
@@ -6,20 +6,22 @@ import React from 'react'
 function TeamCard(props) {
   return (
       <div className='flex flex-col w-fit m-auto p-4 rounded-md group mb-4'>
-          <Image placeholder="blur" blurDataURL="/iconblur.jpg"
-              alt={`${props.name}`}
-              className="transition-transform duration-300 max-w-xs rounded-full object-cover m-auto scale-100  hover:scale-120"
-              height="500"
-              src={urlFor(props.imageSrc).url()}
-              style={{
-                  aspectRatio: "80/80",
-                  objectFit: "cover",
-              }}
-              width="500" />
+          <div className='overflow-hidden rounded-full mb-2'>
+              <Image placeholder="blur" blurDataURL="/iconblur.jpg"
+                  alt={`${props.name}`}
+                  className="transition-transform duration-300 max-w-xs object-cover m-auto scale-100 group-hover:scale-110"
+                  height="500"
+                  src={urlFor(props.imageSrc).url()}
+                  style={{
+                      aspectRatio: "80/80",
+                      objectFit: "cover",
+                  }}
+                  width="500" />
+          </div>
           
           <div className='flex flex-col m-auto text-center'>
               <Link href={`/member/${props.teamMemberSlug}/${props.year}`} className="mb-2 text-[100%] text-[#fcfcfc] font-extrabold">{`${props.name}`}</Link>
-              <p className="mb-4 text-xl text-[#fcfcfc] font-bold">{props.role}</p>
+              <p className="mb-2 text-xl text-[#fcfcfc] font-bold">{props.role}</p>
               <p className="text-sm text-[#fcfcfc]">
                   {props.subtitle}
               </p>
